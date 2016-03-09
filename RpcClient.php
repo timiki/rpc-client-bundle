a7df8432e2937c5f6e83fe725decde8e4b240d56
nikita <nikita@netesin.ru>
2016-03-09 16:02:13 +0800

    Обновление для timiki/rpc-client-common 1.4; 1.1.0;

--- RpcClient.php
+++ RpcClient.php
@@ -3,84 +3,106 @@
 namespace Timiki\Bundle\RpcClientBundle;
 
 use Timiki\RpcClientCommon\Client;
-use \Symfony\Component\DependencyInjection\Container;
 use Timiki\RpcClientCommon\Client\Response;
+use \Symfony\Component\DependencyInjection\ContainerInterface;
+use \Symfony\Component\HttpFoundation\Request;
 
 /**
  * Client class
  */
 class RpcClient extends Client
 {
-    /**
-     * Container
-     *
-     * @var Container
-     */
-    protected $container;
+	/**
+	 * Container
+	 *
+	 * @var ContainerInterface
+	 */
+	protected $container;
 
-    /**
-     * Create new client
-     *
-     * @param null|string|array $address
-     * @param array             $options
-     * @param string            $type
-     * @param string            $locale
-     * @param null|Container    $container
-     */
-    public function __construct($address = null, array $options = [], $type = 'json', $locale = 'en', $container = null)
-    {
-        $this->setContainer($container);
-        parent::__construct($address, $options, $type, $locale);
-    }
+	/**
+	 * Create new client
+	 *
+	 * @param null|string|array $address
+	 * @param array $options
+	 * @param string $type
+	 * @param string $locale
+	 * @param null|ContainerInterface $container
+	 */
+	public function __construct($address = null, array $options = [], $type = 'json', $locale = 'en', ContainerInterface $container = null)
+	{
+		$this->setContainer($container);
 
-    /**
-     * Set container
-     *
-     * @param Container|null $container
-     * @return $this
-     */
-    public function setContainer($container)
-    {
-        if ($container instanceof Container) {
-            $this->container = $container;
-        }
+		$headers = (array)$options['headers'];
+		$cookies = (array)$options['cookies'];
 
-        return $this;
-    }
+		if (is_array($options['forwardHeaders']) and !empty($options['forwardHeaders'])) {
+			foreach ($options['forwardHeaders'] as $header) {
+				$headers[$header] = Request::createFromGlobals()->headers->get($header);
+				if (strtolower($header) == 'client-ip') {
+					$headers[$header] = [Request::createFromGlobals()->getClientIp()];
+				}
+			}
+		}
 
-    /**
-     * Get container
-     *
-     * @return Container|null
-     */
-    public function getContainer()
-    {
-        return $this->container;
-    }
+		if (is_array($options['forwardCookies']) and !empty($options['forwardCookies'])) {
+			foreach (Request::createFromGlobals()->cookies->all() as $name => $values) {
+				if (in_array($name, $options['forwardCookies'])) {
+					$cookies[$name] = $values;
+				}
+			}
+		}
 
-    /**
-     * Call request
-     *
-     * @param string $method
-     * @param array  $params
-     * @param array  $extra
-     * @return Response
-     */
-    public function call($method, array $params = [], array $extra = [])
-    {
-        // Before run call need stop session
-        if ($this->getContainer() !== null) {
-            $this->getContainer()->get('session')->save();
-        }
+		parent::__construct($address, $type, $headers, $cookies, $locale);
+	}
 
-        // Call method
-        $response = parent::call($method, $params, $extra);
+	/**
+	 * Set container
+	 *
+	 * @param ContainerInterface|null $container
+	 * @return $this
+	 */
+	public function setContainer($container)
+	{
+		if ($container instanceof ContainerInterface) {
+			$this->container = $container;
+		}
 
-        // After run call need restart session
-        if ($this->getContainer() !== null) {
-            $this->getContainer()->get('session')->migrate();
-        }
+		return $this;
+	}
 
-        return $response;
-    }
+	/**
+	 * Get container
+	 *
+	 * @return ContainerInterface|null
+	 */
+	public function getContainer()
+	{
+		return $this->container;
+	}
+
+	/**
+	 * Call request
+	 *
+	 * @param string $method
+	 * @param array $params
+	 * @param array $extra
+	 * @return Response
+	 */
+	public function call($method, array $params = [], array $extra = [])
+	{
+		// Before run call need stop session
+		if ($this->getContainer() !== null) {
+			$this->getContainer()->get('session')->save();
+		}
+
+		// Call method
+		$response = parent::call($method, $params, $extra);
+
+		// After run call need restart session
+		if ($this->getContainer() !== null) {
+			$this->getContainer()->get('session')->migrate();
+		}
+
+		return $response;
+	}
 }
